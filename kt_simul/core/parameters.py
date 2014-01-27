2ce518f58f88fd0b2893ffcf6f73d8c7620a8702
HadiM <marelo64@gmail.com>
2014-01-27 16:28:08 +0100

    fix parameters reduction

--- kt_simul/core/parameters.py
+++ kt_simul/core/parameters.py
@@ -69,17 +69,19 @@ def reduce_params(paramtree, measuretree, force_parameters=[]):
     Fk = params['Fk']
 
     #Let's go for the direct relations
-    d0 = obs_d0
     if 'd0' not in force_parameters:
         params['d0'] = obs_d0
+    d0 = params['d0']
 
     Vk = poleward_speed
     if 'Vk' not in force_parameters:
         params['Vk'] = poleward_speed
+    Vk = params['Vk']
 
     Vmz = anaph_rate
     if 'Vmz' not in force_parameters:
         params['Vmz'] = anaph_rate
+    Vmz = params['Vmz']
 
     if 'ldep_balance' not in force_parameters:
         params['ldep_balance'] = mean_kt_spb_dist
@@ -93,17 +95,20 @@ def reduce_params(paramtree, measuretree, force_parameters=[]):
 
     # alpha_mean = float(mean_attachment(k_a/fd_eff) / Mk)
     alpha_mean = 1 / (1 + k_d_eff / k_a)
+
     # Take metaphase kt pair distance as the maximum one
     # TODO : kc = Fk * Mt * alpha_mean / (max_metaph_k_dist - d0)
-    kappa_c = Fk * Mk * 2 / (max_metaph_k_dist - d0)
     if 'kappa_c' not in force_parameters:
-        params['kappa_c'] = kappa_c
+        params['kappa_c'] = Fk * Mk * 2 / (max_metaph_k_dist - d0)
+    kappa_c = params['kappa_c']
 
     #kop = alpha_mean * ( 1 + metaph_rate/2 ) / ( outer_inner_dist )
-    kappa_k = Fk * Mk * 2 / (2 * outer_inner_dist)
+
     if 'kappa_k' not in force_parameters:
-        params['kappa_k'] = kappa_k
-    #Ensure we have sufficientely small time steps
+        params['kappa_k'] = Fk * Mk * 2 / (2 * outer_inner_dist)
+    kappa_k = params['kappa_k']
+
+    # Ensure we have sufficientely small time steps
     dt = params['dt']
     # params['dt'] = min(tau_c / 4., tau_k / 4., params['dt'])
     # if params['dt'] != dt:
@@ -112,11 +117,14 @@ def reduce_params(paramtree, measuretree, force_parameters=[]):
     mus = params['mus']
     Fmz = (Fk * N * Mk * alpha_mean * (1 + metaph_rate / (2 * Vk))
            + mus * metaph_rate / 2.) / (1 - metaph_rate / Vmz)
+
     if 'Fmz' not in force_parameters:
         params['Fmz'] = Fmz
+
     muc = (tau_c * kappa_c)
     if 'muc' not in force_parameters:
         params['muc'] = muc
+
     muk = (tau_k * kappa_k)
     if 'muk' not in force_parameters:
         params['muk'] = muk
